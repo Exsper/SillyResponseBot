2b524f2abc7160bc21a93cab96b0d1d8335e1ff4
arily <lbsaaaaa@gmail.com>
2020-02-01 20:04:12 +0900

    Update findBuilder.js

--- findBuilder.js
+++ findBuilder.js
@@ -4,13 +4,13 @@ const doOrNot = require('./builders/DoOrNot');
 //match the replyObject builder function.
 function findBuilder() {
     this.finder = [{
+            matcher: (s) => s.match(/(.+)[嘛吗呢啊么呀呐](\?)?/) || s.match(/(.+)[嘛吗呢啊么呀呐](\w+)/),
+            builder: doOrNot
+        },
+        {
             matcher: (s) => (s.includes("不") && !s.includes("不不") && s.length <= 30 && s.length > 4),
             builder: AorA
         },
-        {
-            matcher: (s) => s.match(/[嘛吗呢啊么呀呐](\?)?/) || s.match(/[嘛吗呢啊么呀呐](\w+)/),
-            builder: doOrNot
-        }
     ];
 }
 //return false | function
