f50995fb98d892b153474914401986a91183120c
Starlight Exsper <exsper@163.com>
2020-02-02 23:21:14 +0800

    更新随机算法

--- sendMessageObject.js
+++ sendMessageObject.js
@@ -2,15 +2,23 @@
 
 
 // 随机回复抱怨语句
-let botherLeastTimes = 3; // 相同提问第3次开始就回复抱怨语句
-let refuseReplyTimes = 10; // 相同提问大于10次不再回复
+const botherLeastTimes = 3; // 相同提问第3次开始就回复抱怨语句
+const refuseReplyTimes = 10; // 相同提问大于10次不再回复
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function replyNoBother(times) {
     let replys = [
-        "别再问我啦", "你好烦啊", "讨厌！", "别问啦！", "不作回答", "...", "？", "你还问！", "好烦呀！", "不想回答",
-        "你是复读机吗？", "这个问题我拒绝回答", "哼！", "我生气啦！"
+        "别再问我啦", "你已经问了好多遍了", "听不见吗", "还听不见吗", "别问啦！", "够啦！",
+        "...", "？", "你好烦啊", "讨厌！", "不作回答", "你还问！", "好烦呀！", "哼！",
+        "不想回答", "你是复读机吗？", "这个问题我拒绝回答", "我生气啦！", "这里有个聋子！",
+        "小阿日对你的好感度-100", "我要拿胶布把你的嘴堵上", "你再怎么问都不会有其他结果的",
+        "zzz", "但是我拒绝", "您已被管理员禁言", "您已被移出群聊", "核弹已升空"
     ];
-    if (times < refuseReplyTimes) return replys[Math.floor(Math.random() * replys.length)];
+    // times越大，越可能选到靠后的语句（算法还待改进？
+    if (times < refuseReplyTimes) return replys[Math.floor(getRndInteger((1 - botherLeastTimes / times) * replys.length, Math.sqrt(times / (refuseReplyTimes - 1)) * replys.length))];
     else if (times === refuseReplyTimes) return "小阿日不理你了";
     else return "";
 }
